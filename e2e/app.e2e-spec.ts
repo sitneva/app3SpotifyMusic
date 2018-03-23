bb3a25f2065130df5979fea7166d4e53d6cbde8d
sitneva <sitneva.t.v@gmail.com>
2018-03-23 14:05:52 +0200

    created new empty proj for Spotify

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('app3-spotify-music App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
